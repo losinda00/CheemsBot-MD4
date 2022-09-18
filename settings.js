766c1d68a80059f5786c48798ef554be5c0406e6
losinda00 <111949529+losinda00@users.noreply.github.com>
2022-09-19 00:12:05 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.vcardowner = ['212777466518'] //ur owner number
+global.ownername = "𝒂𝒍𝒗𝒂𝒍𝒐𝒔𝒊" //ur owner name
+global.ytname = "𝒎3𝒏𝒅𝒄𝒉" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['212777466518']
+global.ownertag = '212777466518' //ur tag number
+global.botname = '𝙖𝙡𝙫𝙖𝙡𝙤𝙨𝙞😻'//ur bot name
+global.linkz = "https://wa.me/212777466518" //your theme url which will be displayed on whatsapp
+global.websitex = "https://wa.me/212777466518" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.reactmoji = "😻" //ur menu react emoji
+global.themeemoji = "😹" //ur theme emoji
+global.packname = "𝐀𝐋𝐕𝐀𝐑𝐎⃢🌹ٱࢦأمࢪيــڪي/𝐋𝐎𝐒𝐈𝐍𝐃𝐀⃢🌹ٱࢦأمࢪيــڪية " //ur sticker watermark packname
+global.author = "𝐲𝐨𝐮 𝐚𝐫𝐞 𝐞𝐯𝐞𝐫𝐲𝐭𝐡𝐢𝐧𝐠 𝐦𝐲 𝐚𝐧𝐠𝐞𝐥😻😽" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['212777466518'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -106,14 +106,14 @@ global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
+    admin: 'غي الأدمن لي كيديرها',
     botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
+    owner: 'غي الفارو لي يديرها!',
     group: 'Feature Used Only For Groups!',
     private: 'Features Used Only For Private Chat!',
     bot: 'This Feature Is Only For Bot',
     wait: 'In process...',
-    linkm: 'Where is the link?',
+    linkm: 'فين هوا ليان؟ ',
     error: 'Error!!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
